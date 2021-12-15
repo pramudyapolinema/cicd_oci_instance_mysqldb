b2451795cab9bf86071a145f4df329291abad1e2
pramudyapolinema <1941720054@student.polinema.ac.id>
2021-12-15 13:40:00 +0700

    rev

--- laravel.sh
+++ laravel.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-docker exec -it system_sales bash -c "
 cp .env.example .env
 chmod -R 777 /var/www/html/storage/; \
 composer install; \
 php artisan key:generate; \
 php artisan migrate; \
 php artisan db:seed --class=LocationSeeder; \
-php artisan db:seed --class=UserSeeder"
+php artisan db:seed --class=UserSeeder
